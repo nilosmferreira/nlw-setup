19ee0de85e834b8e9f891bbca339e1e537eaff0f
Nilo Ferreira <nilo_ferreira@hotmail.com>
2023-01-19 17:57:20 -0300

    meio da segunda aula

---
+++ server/src/routes.ts
@@ -0,0 +1,56 @@
+import { FastifyInstance } from 'fastify';
+import daysjs from 'dayjs';
+import { prisma } from './lib/prisma';
+import { z } from 'zod';
+
+export async function appRoutes(app: FastifyInstance) {
+  app.post('/habits', async (req) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+
+    const { title, weekDays } = createHabitBody.parse(req.body);
+
+    const today = daysjs().startOf('day').toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            };
+          }),
+        },
+      },
+    });
+  });
+  app.get('/day', async (req) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(req.query);
+
+    const weekDate = daysjs(date).get('day');
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDate,
+          },
+        },
+      },
+    });
+    return {
+      possibleHabits,
+    };
+  });
+}
