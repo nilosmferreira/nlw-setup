00dceb974f601b23bb63e5bd9893de5e5dad1690
Nilo Ferreira <nilo_ferreira@hotmail.com>
2023-01-19 14:39:23 -0300

    feat: mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,42 @@
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter';
+import { Loading } from './src/components/loading';
+
+export default function App() {
+  const [fontsLoader] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+  if (!fontsLoader) {
+    return <Loading />;
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar barStyle={'light-content'} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#09090A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#FFF',
+    // fontFamily: 'Inter_800ExtraBoldr',
+  },
+});
